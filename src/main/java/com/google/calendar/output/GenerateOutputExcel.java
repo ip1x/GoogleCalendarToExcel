5acf438fea30b9cfd0011deb09d2f291eeba6e4a
Rohit Aggarwal <rohita@damcogroup.com>
2017-03-27 12:19:28 +0530

    Changes made for the template file of excel

--- src/main/java/com/google/calendar/output/GenerateOutputExcel.java
+++ src/main/java/com/google/calendar/output/GenerateOutputExcel.java
@@ -1,31 +1,52 @@
 package com.google.calendar.output;
 
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import com.google.common.io.Files;
 
 public class GenerateOutputExcel {
 
-	private XSSFWorkbook workbook;
-	private XSSFSheet sheet;
+	private Workbook workbook;
+	private Sheet sheet;
+	private static String DESTINATION_FILE_PATH = "Z:/calendar.xlsx";
 
 	public GenerateOutputExcel() {
-		workbook = new XSSFWorkbook();
-		sheet = workbook.createSheet("Google calendar");
 	}
 
-	public XSSFWorkbook getWorkbook() {
+	public void generateExcelFile() throws Exception {
+		copyTemplateFile();
+		workbook = WorkbookFactory.create(new FileInputStream(DESTINATION_FILE_PATH));
+		sheet = workbook.getSheetAt(0);
+	}
+
+	private void copyTemplateFile() throws URISyntaxException, IOException {
+		final File newFile = new File(DESTINATION_FILE_PATH);
+		final URL url = Main.class.getClassLoader().getResource("template.xls");
+		final File templateFile = new File(url.toURI());
+		Files.copy(templateFile, newFile);
+	}
+
+	public Workbook getWorkbook() {
 		return workbook;
 	}
 
-	public void setWorkbook(final XSSFWorkbook workbook) {
+	public void setWorkbook(final Workbook workbook) {
 		this.workbook = workbook;
 	}
 
-	public XSSFSheet getSheet() {
+	public Sheet getSheet() {
 		return sheet;
 	}
 
-	public void setSheet(final XSSFSheet sheet) {
+	public void setSheet(final Sheet sheet) {
 		this.sheet = sheet;
 	}
 }
--- src/main/java/com/google/calendar/util/ExcelFormat.java
+++ src/main/java/com/google/calendar/util/ExcelFormat.java
@@ -2,54 +2,54 @@ package com.google.calendar.util;
 
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.CreationHelper;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFFont;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class ExcelFormat {
 
-	public static CellStyle getBoldCellStyle(final XSSFWorkbook workBook) {
-		final XSSFCellStyle cellStyle = workBook.createCellStyle();
-		final XSSFFont font = workBook.createFont();
+	public static CellStyle getBoldCellStyle(final Workbook workBook) {
+		final CellStyle cellStyle = workBook.createCellStyle();
+		final Font font = workBook.createFont();
 		font.setBold(true);
 		cellStyle.setFont(font);
 		return cellStyle;
 	}
 
-	public static CellStyle getDateCellType(final XSSFWorkbook workBook) {
-		final XSSFCellStyle cellStyle = workBook.createCellStyle();
+	public static CellStyle getDateCellType(final Workbook workBook) {
+		final CellStyle cellStyle = workBook.createCellStyle();
 		final CreationHelper createHelper = workBook.getCreationHelper();
 		cellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd/mm/yyyy"));
 		return cellStyle;
 	}
 
-	public static CellStyle getLeftAllignCellType(final XSSFWorkbook workBook) {
-		final XSSFCellStyle cellStyle = workBook.createCellStyle();
-		cellStyle.setAlignment(CellStyle.ALIGN_LEFT);
+	public static CellStyle getLeftAllignCellType(final Workbook workBook) {
+		final CellStyle cellStyle = workBook.createCellStyle();
+		cellStyle.setAlignment(HorizontalAlignment.LEFT);
 		return cellStyle;
 	}
 
-	public static CellStyle getRightAllignCellType(final XSSFWorkbook workBook) {
-		final XSSFCellStyle cellStyle = workBook.createCellStyle();
-		cellStyle.setAlignment(CellStyle.ALIGN_RIGHT);
+	public static CellStyle getRightAllignCellType(final Workbook workBook) {
+		final CellStyle cellStyle = workBook.createCellStyle();
+		cellStyle.setAlignment(HorizontalAlignment.RIGHT);
 		return cellStyle;
 	}
 
-	public static CellStyle getRightAllignAndBoldCellType(final XSSFWorkbook workBook) {
+	public static CellStyle getRightAllignAndBoldCellType(final Workbook workBook) {
 		final CellStyle cellStyle = getBoldCellStyle(workBook);
-		cellStyle.setAlignment(CellStyle.ALIGN_RIGHT);
+		cellStyle.setAlignment(HorizontalAlignment.RIGHT);
 		return cellStyle;
 	}
 
-	public static CellStyle getLeftAllignAndDateCellType(final XSSFWorkbook workBook) {
+	public static CellStyle getLeftAllignAndDateCellType(final Workbook workBook) {
 		final CellStyle cellStyle = getDateCellType(workBook);
-		cellStyle.setAlignment(CellStyle.ALIGN_LEFT);
+		cellStyle.setAlignment(HorizontalAlignment.LEFT);
 		return cellStyle;
 	}
 
-	public static CellStyle getHeightAndBoldCellType(final XSSFWorkbook workBook) {
-		final XSSFCellStyle cellStyle = (XSSFCellStyle) getBoldCellStyle(workBook);
-		cellStyle.getFont().setFontHeight(20);
-		return cellStyle;
-	}
+	//	public static CellStyle getHeightAndBoldCellType(final Workbook workBook) {
+	//		final CellStyle cellStyle = getBoldCellStyle(workBook);
+	//		cellStyle.getFont().setFontHeight(20);
+	//		return cellStyle;
+	//	}
 }
